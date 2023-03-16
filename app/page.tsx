56ed1620963581625ec8b9faf2f29a5b7db13338
Thomas Couderc <thomas.couderc@gmail.com>
2023-03-16 18:30:33 +0100

    refacto: removed unnecessary home

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,46 @@
-import React from 'react';
-import HomePage from '@/app/home';
+import '../styles/globals.css';
+
+import About from '@/app/about';
+import Headers from '@/app/header';
+import Github from './github';
+import Contact from './contact';
+import Studies from './studies';
+import Frameworks from './frameworks';
+import Skills from './skills';
+import Learnings from './learnings';
 
 export default async function Page() {
   return (
     <>
-      <HomePage />
+      {/* @ts-expect-error Server Component */}
+      <Headers />
+      {/* @ts-expect-error Server Component */}
+      <About />
+      {/* @ts-expect-error Server Component */}
+      <Skills />
+
+      <div className="mt-20 flex">
+        <div id="left" className="w-1/3 pr-10">
+          {/* @ts-expect-error Server Component */}
+          <Contact />
+          {/* @ts-expect-error Server Component */}
+          <Github />
+          {/* @ts-expect-error Server Component */}
+          <Frameworks />
+          {/* @ts-expect-error Server Component */}
+          <Learnings />
+
+          {/* <Interests /> */}
+        </div>
+        <div id="main" className="w-4/6">
+          {/* <Projects /> */}
+
+          {/* <Experiences /> */}
+
+          {/* @ts-expect-error Server Component */}
+          <Studies />
+        </div>
+      </div>
     </>
   );
 }
