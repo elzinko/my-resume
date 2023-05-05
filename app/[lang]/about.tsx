406bba75f4a3b140725ba616767345244930ba81
Thomas Couderc <thomas.couderc@gmail.com>
2023-05-05 17:55:07 +0200

    added about section to i18n

--- app/[lang]/about.tsx
+++ app/[lang]/about.tsx
@@ -1,10 +1,11 @@
 import { graphQLClient } from '@/lib/graphql-client';
 import { gql } from 'graphql-request';
+import { Locale } from 'i18n-config';
 import React from 'react';
 
 const query = gql`
-  {
-    about {
+  query getAbout($lang: SiteLocale) {
+    about(locale: $lang) {
       id
       title
       text
@@ -12,14 +13,17 @@ const query = gql`
   }
 `;
 
-async function getData() {
-  const data: any = await graphQLClient.request(query);
+async function getData(locale: any) {
+  const variables = {
+    lang: locale.locale,
+  };
+  const data: any = await graphQLClient.request(query, variables);
   console.log(data);
   return data;
 }
 
-export default async function About() {
-  const data: any = await getData();
+export default async function About(locale: Locale) {
+  const data: any = await getData(locale);
   return (
     <section id="about" className="mt-10">
       <h2 className="border-b pb-1 text-2xl font-semibold text-teal-300">
