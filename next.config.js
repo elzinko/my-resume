668f42c1898c88fb6909f7801d35188d5d425c44
Thomas Couderc <thomas.couderc@gmail.com>
2023-05-09 17:24:04 +0200

    ci: add static generation

--- next.config.js
+++ next.config.js
@@ -9,7 +9,12 @@ const nextConfig = {
   },
   experimental: {
     appDir: true,
-  }
+  },
+  output: 'export',
+  //   // Optional: Add a trailing slash to all paths `/about` -> `/about/`
+  //   // trailingSlash: true,
+  //   // Optional: Change the output directory `out` -> `dist`
+  //   // distDir: 'dist',
 }
 
 module.exports = nextConfig
