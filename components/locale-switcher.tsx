acb6ea9bc9d6534913c0ac5800aae5188bdd9384
Thomas Couderc <thomas.couderc@gmail.com>
2023-09-12 11:49:39 +0200

    ci: better debug for github deploy

--- components/locale-switcher.tsx
+++ components/locale-switcher.tsx
@@ -3,17 +3,30 @@
 import { usePathname } from 'next/navigation';
 import Link from 'next/link';
 import { i18n } from '../i18n-config';
+import path from 'path';
 
 export default function LocaleSwitcher(lang: any) {
-  const pathName = usePathname();
-  console.log('LS : pathName : ' + pathName);
+  const basePath = process.env.NEXT_PUBLIC_BASE_PATH || '';
+  let pathName = usePathname();
+
+  console.log('LS : basePath : ' + basePath);
+  console.log('LS : pathName before cleaning : ' + pathName);
+
+  // remove basePath from pathName if it exists
+  if (basePath != '') {
+    pathName = pathName.replace(basePath, '');
+  }
+  console.log('LS : pathName after cleaning : ' + pathName);
+
   const redirectedPathName = (locale: string) => {
-    if (!pathName) return '/';
-    console.log('LS : no path => return / ');
+    if (!pathName) {
+      console.log('LS : no path => return / ');
+      return '/';
+    }
     const segments = pathName.split('/');
     segments[1] = locale;
     const newPathName = segments.join('/');
-    console.log('LS : existing path => new path = ' + newPathName);
+    console.log('LS : has path => new path = ' + newPathName);
     return newPathName;
   };
 
