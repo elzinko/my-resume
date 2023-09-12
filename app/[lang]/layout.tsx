d98777dd359200861e50023ac502e777970468b1
Thomas Couderc <thomas.couderc@gmail.com>
2023-09-12 13:56:48 +0200

    fix: disable anaytics for static exports

--- app/[lang]/layout.tsx
+++ app/[lang]/layout.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import { i18n } from '../../i18n-config'
+import { i18n } from '../../i18n-config';
 import { Analytics } from '@vercel/analytics/react';
 
 export async function generateStaticParams() {
-  return i18n.locales.map((locale) => ({ lang: locale }))
+  return i18n.locales.map((locale) => ({ lang: locale }));
 }
 
 export default async function RootLayout({
@@ -11,13 +11,22 @@ export default async function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
+  let debug = true;
+  if (
+    process.env.NEXT_PUBLIC_VERCEL_ENV === 'production' ||
+    process.env.NEXT_PUBLIC_EXPORT != ''
+  ) {
+    debug = false;
+  }
+  console.log('Analytics debug = ' + debug);
   return (
     <html lang="fr">
       <body>
         <div className="container mx-auto min-h-screen p-8">
           <main>{children}</main>
         </div>
-        <Analytics />
+        {/* if debug is true, then add Analytics */}
+        if (debug) {<Analytics debug={debug} />}
       </body>
     </html>
   );
